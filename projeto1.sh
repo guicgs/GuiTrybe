3f1b68192d55ba5f29c68b6e97ee898d60cdfcf5
guicgs <etohlif@protonmail.com>
2019-11-05 19:12:32 -0300

    primeiro commit

---
+++ projeto1.sh
@@ -0,0 +1,10 @@
+read MESSAGE
+
+if [[ -z $MESSAGE ]]; then
+    echo "Ops, você precisa passar um mensagem como argumento!"
+    exit 1
+else
+    curl -X POST -H 'Content-type: application/shellscript' --data "{\"text\":\"$MESSAGE\"}" https://hooks.slack.com/services/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
+fi
+
+#curl -X POST -H 'Content-type: application/json' --data '{"text":"Olá Mundo"}' YOUR_WEBHOOK_URL https://hooks.slack.com/services/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
